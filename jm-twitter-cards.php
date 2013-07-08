3402f8f5eec6ea0e9814fcc5079b626b41c3e425
jmlapam <jmlapam@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-07-08 13:47:18 +0000

    add option to select thumbnail size
    git-svn-id: https://plugins.svn.wordpress.org/jm-twitter-cards/trunk@737749 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- jm-twitter-cards.php
+++ jm-twitter-cards.php
@@ -5,15 +5,16 @@ Plugin URI: http://tweetpress.fr
 Description: Meant to help users to implement and customize Twitter Cards easily
 Author: Julien Maury
 Author URI: http://tweetpress.fr
-Version: 3.2.0
+Version: 3.2.1
 License: GPL2++
 */
 
 /*
 *    Sources: - https://dev.twitter.com/docs/cards
 * 			  - http://codex.wordpress.org/Function_Reference/wp_enqueue_style
-*			  - https://github.com/rilwis/meta-box
+*			  - https://github.com/rilwis/meta-box [GREAT]
 *			  - http://codex.wordpress.org/Function_Reference/wp_get_attachment_image_src
+*			  - https://github.com/trepmal/featured-image-sizes/blob/v1/featured-image-sizes.php [GREAT]
 */
 
 
@@ -54,6 +55,98 @@ function jm_tc_remove_spaces($space) {
 	$nospace =  strip_shortcodes(  strip_tags( preg_replace("/\s+/", " ", $space) )  );// great regex by @aarontgrogg tips http://tweetpress.fr/plugin/jm-twitter-cards/comment-page-3#comment-618
 	return $nospace;
 }
+
+
+// Very simple class made by @trepmal but pretty handy
+$featured_image_sizes = new Featured_Image_Sizes();
+
+class Featured_Image_Sizes {
+
+	var $textdomain = 'jm-tc';
+
+	function __construct() {
+
+		global $_wp_post_type_features;
+		$possible_post_types = wp_list_filter( $_wp_post_type_features, array( 'thumbnail' => 1 ) );
+
+		$this->allowed_post_types = apply_filters( 'fis_post_types', $possible_post_types );
+
+		add_filter( 'admin_post_thumbnail_html', array( &$this, 'admin_post_thumbnail_html' ), 10, 2 );
+		add_action( 'save_post', array( &$this, 'save_post' ), 10, 2 );
+
+		add_filter( 'post_thumbnail_size', array( &$this, 'post_thumbnail_size' ) );
+
+	}
+
+	function admin_post_thumbnail_html( $content, $post_id ) {
+
+		$post_type = get_post_type( $post_id );
+		if ( ! isset( $this->allowed_post_types[ $post_type ] ) ) return $content;
+
+		ob_start();
+		wp_nonce_field( 'na-fis-image', 'nn-fis-image' );
+		$saved_value = get_post_meta( $post_id, 'fis-image-size', true );
+
+		echo '<label>';
+		_e( 'Choose a size for this image to be displayed at', $this->textdomain );
+		echo '<select name="fis-image-size">';
+		echo '<option value="">' . __( 'Depend on theme setting', $this->textdomain ) .'</option>';
+
+		foreach( get_intermediate_image_sizes() as $size ) {
+			$selected = selected( $size, $saved_value, false );
+			echo "<option value='$size'$selected>$size</option>";
+		}
+		echo '</select></label>';
+
+		$html = ob_get_clean();
+
+		return $content . $html;
+	}
+
+	function save_post( $post_id, $post ) {
+
+		if ( ! isset( $_POST['nn-fis-image'] ) ) //make sure our custom value is being sent
+			return;
+		if ( ! wp_verify_nonce( $_POST['nn-fis-image'], 'na-fis-image' ) ) //verify intent
+			return;
+		if ( defined('DOING_AUTOSAVE') && DOING_AUTOSAVE ) //no auto saving
+			return;
+		if ( ! current_user_can( 'edit_post', $post_id ) ) //verify permissions
+			return;
+
+		$fis_size = trim( $_POST['fis-image-size'] );
+		if ( empty( $fis_size ) ) {
+			delete_post_meta( $post_id, 'fis-image-size' );
+			return;
+		}
+
+		// make sure the POSTed value is an okay size name
+		$ok_sizes = get_intermediate_image_sizes();
+		if ( ! in_array( $fis_size, $ok_sizes ) ) return;
+
+		update_post_meta( $post_id, 'fis-image-size', $fis_size );
+
+	}
+
+	function post_thumbnail_size( $size ) {
+
+		// make sure we're in the main loop
+		// so as not to interfere with secondary loops (perhaps widgets)
+		global $wp_query;
+		if ( ! $wp_query->in_the_loop ) return $size;
+
+		// var_dump( get_the_ID() );
+
+		$fis_size = get_post_meta( get_the_ID(), 'fis-image-size', true );
+
+		if ( empty( $fis_size ) ) return $size;
+
+		return $fis_size;
+	}
+
+}
+
+
 
 // grab our datas
 $opts = jm_tc_get_options();          
@@ -260,6 +353,7 @@ if(!function_exists( 'add_twitter_card_info' )) {
 			$cardLabel1 = get_post_meta(get_the_ID(),'cardLabel1',true);
 			$cardData2 = get_post_meta(get_the_ID(),'cardData2',true);
 			$cardLabel2 = get_post_meta(get_the_ID(),'cardLabel2',true);
+			$fis_size = get_post_meta( get_the_ID(), 'fis-image-size', true );
 			
 
 			// support for custom meta description WordPress SEO by Yoast or All in One SEO
@@ -296,7 +390,7 @@ if(!function_exists( 'add_twitter_card_info' )) {
 			echo '<meta name="twitter:description" content="' . jm_tc_remove_spaces($cardDescription) . '">'."\n"; 
 
 			if(get_the_post_thumbnail( $post->ID ) && empty($cardImage)) { //featured image is set but not cardImage
-				$thumb = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'medium' );
+				$thumb = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ),$fis_size);
 				echo '<meta name="twitter:image" content="' . $thumb[0] . '">'."\n";
 			} elseif( !empty($cardImage) ) { // cardImage is set
 				echo '<meta name="twitter:image" content="' . $cardImage . '">'."\n";
